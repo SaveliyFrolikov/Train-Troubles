2550aeeefafaa2510b7aec3eac007644360a052a
SaveliyFrolikov <thecoder13oops@gmail.com>
2021-06-23 17:48:41 +0300

    CheckPoint 006
    Поезд едет по рельсам

---
+++ Train Troubles/Assets/Scripts/Train.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float speed;
+    float singleStep;
+    float step;
+    bool newPointFound;
+    GameObject target;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        singleStep = speed * Time.deltaTime;
+        step = speed * Time.deltaTime;
+
+        if (target.transform.parent.parent.GetComponent<Rail>().isPlaced)
+        {
+            Vector3 targetPos = target.gameObject.transform.position - transform.position;
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetPos, singleStep, 0);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "WayPoint")
+        {
+            target = other.gameObject;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+}
