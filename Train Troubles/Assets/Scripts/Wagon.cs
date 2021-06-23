7b7176ec140b9a1117998a4c4dcad30cd74c7b40
SaveliyFrolikov <thecoder13oops@gmail.com>
2021-06-23 21:49:47 +0300

    CheckPoint 007
    Исправлены баги и добавлены вагоны

--- Train Troubles/Assets/Scripts/Wagon.cs
+++ Train Troubles/Assets/Scripts/Wagon.cs
@@ -2,23 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Train : MonoBehaviour
+public class Wagon : MonoBehaviour
 {
-
     [SerializeField] float rotationSpeed;
     [SerializeField] float speed;
     [SerializeField] float timer;
     float singleStep;
     float step;
-    
-    GameObject target;
-   
+    [SerializeField] float offset;
+    [SerializeField] GameObject train;
+    [SerializeField] GameObject target;
+    bool stop = false;
+    Vector3 current;
+    Vector3 past;
+
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         if (timer > 0)
@@ -27,16 +30,29 @@ public class Train : MonoBehaviour
         }
         else
         {
+            current = train.transform.position;
+
+            if (current != past)
+            {
+                stop = false;
+            }
+            else
+            {
+                stop = true;
+            }
+            past = current;
+
             timer = 0;
             singleStep = speed * Time.deltaTime;
             step = speed * Time.deltaTime;
 
-            if (target.transform.parent.parent.GetComponent<Rail>().isPlaced)
+            if (!stop)
             {
                 Vector3 targetPos = target.gameObject.transform.position - transform.position;
                 Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetPos, singleStep, 0);
                 transform.rotation = Quaternion.LookRotation(newDirection);
                 transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+
             }
         }
     }
